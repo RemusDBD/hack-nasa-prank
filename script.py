9bba1458872cc0f0b2b2afa101437321294d5296
Remus <127229497+RemusDBD@users.noreply.github.com>
2024-04-01 02:03:22 +0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,64 @@
+import time
+
+banner = r"""
+   __                              __   
+  /__\ ___ _ __ ___  _   _ ___    / /   
+ / \/// _ \ '_ ` _ \| | | / __|  / /    
+/ _  \  __/ | | | | | |_| \__ \ / /____ 
+\/ \_/\___|_| |_| |_|\__,_|___/ \____(_)
+
+
+"""
+print(banner)
+    
+a = (r"""
+C:\Windows\System32\>whoami
+nasa\admin
+""")
+
+b = ("""Shell : localhost:6969""")
+
+c = (r"""C:\Windows\System32\>dir 
+Volume in drive C has no label.
+Volume Serial Number is NASA-DISK
+
+Directory of C:\Windows\System32\
+
+1/4/2024  00:00 AM <DIR>  .
+1/4/2024  00:00 AM <DIR> ..
+1/4/2024  00:00 AM 0 flag.txt
+""")
+
+d = ("""Happy April Fool's Day""")
+
+print("Start Attacking NASA....")
+time.sleep(1);
+print("Deploying Reverse Shell in NASA Server....")
+time.sleep(1);
+print("Nmap for opening port...")
+time.sleep(1);
+print("SSH Port Open and seems vulnerable...")
+time.sleep(1);
+print("Hacking NASA 0%");
+time.sleep(1);
+print("Hacking NASA 20%");
+time.sleep(1);
+print("Hacking NASA 40%");
+time.sleep(1);
+print("Hacking NASA 60%");
+time.sleep(1);
+print("Hacking NASA 80%");
+time.sleep(1);
+print("Hacking NASA 100%");
+time.sleep(2);
+print(a);
+time.sleep(2);
+print(b);
+time.sleep(2);
+print("NASA Hacked Successfully");
+
+# Input area
+input_command = input(r"C:\Windows\System32\>")
+print(c)
+input_command = input(r"C:\Windows\System32\>")
+print(d)
